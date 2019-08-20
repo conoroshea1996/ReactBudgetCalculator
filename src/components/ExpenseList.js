cb4f3ac8bfc83650554401c77d74cd973322772c
conoroshea1996 <conoshea12@gmail.com>
2019-08-20 21:27:33 +0100

    first commit

---
+++ src/components/ExpenseList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Item from './ExpenseItem';
+
+const ExpenseList = () => {
+    return (
+        <div>
+            Expense List
+            <Item />
+        </div>
+    )
+}
+
+export default ExpenseList
